98d146951b3b76787c2f6b86e1ae236e256da667
danielhp12123 <daniel.hernandezpina@campusfp.es>
2025-06-13 13:54:22 +0200

    Actulizado crear_tabla.sql

--- src/main/java/sql/crear_tabla.sql
+++ src/main/java/sql/crear_tabla.sql
@@ -1,7 +1,7 @@
-# drop database if exists akihabara_db;
+drop database if exists akihabara_db;
 Create database akihabara_db;
 Use akihabara_db;
-# drop table if exists productos;
+drop table if exists productos;
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
@@ -18,6 +18,8 @@ CREATE TABLE clientes (
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+select * from productos;
+select * from clientes;
 # Insert para productos
 INSERT INTO productos (nombre, categoria, precio,stock) VALUES 
 ("Figura de Anya Forger", "Figura", 59.95, 8),
@@ -28,3 +30,12 @@ INSERT INTO clientes (nombre, email, telefono) VALUES
 ('Daniel Henandez', 'daniel@campusfp.es', '+34 612 345 678'),
 ('Esteban martin', 'Esteban@campusfp.es', '+34 699 123 456'),
 ('Mondongo goku', 'Mondongo.goku@example.com', '+34 620 987 654');
+
+-- Crear el usuario (si no existe)
+CREATE USER IF NOT EXISTS 'DHP_akihabara'@'localhost' IDENTIFIED BY 'campusfp';
+
+-- Darle todos los permisos sobre la base de datos akihabara_db
+GRANT ALL PRIVILEGES ON akihabara_db.* TO 'DHP_akihabara'@'localhost';
+
+-- Aplicar los cambios de permisos
+FLUSH PRIVILEGES;
